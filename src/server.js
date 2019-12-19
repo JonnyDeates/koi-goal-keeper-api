1952186de7e5283c6d4736a79a2ce1bac148645d
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2019-12-18 21:19:20 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const app = require('./app');
-const { PORT, DATABASE_URL, NODE_ENV } = require('./config')
+const { PORT, DATABASE_URL, NODE_ENV } = require('./config');
 const knex = require('knex');
 
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
-})
+});
 
-app.set('db', db)
+app.set('db', db);
 app.listen(PORT, () => {
     if (NODE_ENV !== 'production') {
         console.log(`Server listening at http://localhost:${PORT}`);
