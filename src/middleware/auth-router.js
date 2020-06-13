4519f18ad227ef0e4d0a2aeacb7bac11b8f799c9
Jonny Deates <48420377+JonnyDeates@users.noreply.github.com>
2020-06-13 16:27:25 -0600

    Update auth-router.js

--- src/middleware/auth-router.js
+++ src/middleware/auth-router.js
@@ -12,7 +12,7 @@ const validateEmail = (email) => {
 const encrypt = (string) => {
     let x = 0;
     for(let i = 0; i< string.length; i++){
-        x = x + string.charCodeAt(i)
+        x = x + string.fromCharCode(i)
     }
     return x*7008041*7007177*7005451*7004237;
 };
