348f96392f559774a206b881d1972a5c068ddd13
Jonny Deates <jonnydeates@gmail.com>
2021-03-25 23:13:32 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://Jonny:CheggSpy@localhost/koigoalkeeper',
     JWT_SECRET: process.env.JWT_SECRET || 'kingpendata',
     GOOGLE_SECRET: process.env.GOOGLE_SECRET || 75000,
-    STRIPE_SECRET: process.env.STRIPE_SECRET || "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",
+    STRIPE_SECRET: process.env.STRIPE_SECRET || "sk_test_51HKB0JJUCsOYSAkZZW6HhkQn68s8tbfSE6eWSqnNr14fVJtxXJ0TPn5LhRtLtEXbiN9nnEKFxA2WAjA9qBfxqhrj00dgawHSJM",
     EMAIL_SECRET: process.env.EMAIL_SECRET || 'password123!'
 };
