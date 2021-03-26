f9860107791c91a8ded7852bb662ba18a72dbcac
Jonny Deates <jonnydeates@gmail.com>
2021-03-25 23:31:35 -0500

    please work

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 const app = require('./app');
 const { PORT, DATABASE_URL, NODE_ENV } = require('./config');
 const knex = require('knex');
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-      }
 });
 
 app.set('db', db);
